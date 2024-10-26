29cbbc0878e76c1162fe0efd9b109488a33931b9
devChrist-js <christiankenfack12@gmail.com>
2024-10-26 12:12:24 +0100

    Install and configure Convict

---
+++ convict-config.ts
@@ -0,0 +1,59 @@
+import convict from 'convict';
+
+// Define a schema
+export const config = convict({
+    env: {
+        doc: 'The application environment.',
+        format: ['production', 'development', 'staging'],
+        default: 'development',
+        env: 'PROJECT_ENV'
+    },
+
+    port: {
+        doc: 'The port to bind.',
+        format: 'port',
+        default: 3000,
+        env: 'PORT',
+        arg: 'port'
+    },
+    host: {
+        doc: 'Application host.',
+        format: '*',
+        default: 'localhost',
+    },
+    db: {
+        host: {
+            doc: 'Database host name/IP',
+            format: '*',
+            default: '127.0.0.1:27017',
+            env: 'DB_MONGO_HOST'
+        },
+        name: {
+            doc: 'Database name',
+            format: String,
+            default: '',
+            env: 'DB_MONGO_NAME'
+        },
+
+    },
+    baseUrl: {
+        doc: 'API base url.',
+        format: String,
+        default: '',
+        env: 'BASE_URL',
+        arg: 'base-url'
+    },
+    basePath: {
+        doc: 'API base path.',
+        format: String,
+        default: ''
+    },
+});
+// Load environment dependent configuration
+const env = config.get('env');
+config.loadFile('./src/envs/' + env + '.json');
+
+// Perform validation
+config.validate({ allowed: 'strict' });
+
+export default config;
